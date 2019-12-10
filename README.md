217ccc3c9dd4987ba3935d36db190c32d1f97f4c
Kingller Tang <kingller.tang@gaiaworks.cn>
2019-12-10 18:46:31 +0800

    A webpack plugin that splits entry file into multiple files and add them to entry of webpack

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
 # entry-chunk-webpack-plugin
 A webpack plugin that splits entry file into multiple files and add them to entry of webpack.
+
+
+## Install
+
+```bash
+npm i -D entry-chunk-webpack-plugin
+``` 
+
+
+## Usage
+
+In your webpack configuration (`webpack.config.js`):
+
+```javascript
+const EntryChunkWebpackPlugin = require('entry-chunk-webpack-plugin');
+const LibraryExtendWebpackPlugin = require('library-extend-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: {
+        example: './src/index.ts',
+        'example.min': './src/index.ts',
+    },
+    output: {
+        library: "LIB",
+        libraryTarget: "umd"
+    },
+    //...
+    plugins: [
+        new EntryChunkWebpackPlugin({
+            chunkConfig: [{
+                name: 'example.base',
+            }, {
+                name: 'example.extend',
+                modules: ['RichEditor', 'ReactCropper']
+            }],
+        }),
+        new LibraryExtendWebpackPlugin({
+            exclude: function (fileName) {
+                return !/\.[tj]s$/i.test(fileName) || /example(\.base)*(\.min)*\.js/.test(fileName);
+            }
+        }),
+    ]
+}
+```
+
+
+### Entry File: `index.ts`
+
+```javascript
+export { default as Avatar } from './components/avatar/index';
+
+export { default as Button } from './components/button/button';
+
+export { default as RichEditor } from './components/rich-editor/index';
+
+export { default as ReactCropper } from './components/react-cropper/index';
+
+```
+
+
+### Output Bundle
+
+It will output 6 files
+```javascript
+example.js ({ Avatar, Button, RichEditor, ReactCropper })
+example.min.js ({ Avatar, Button, RichEditor, ReactCropper })
+example.base.js ({ Avatar, Button })
+example.base.min.js ({ Avatar, Button })
+example.extend.js ({ RichEditor, ReactCropper })
+example.extend.min.js ({ RichEditor, ReactCropper })
+```
+
+`Avatar` and `Button`  will be packaged to `example.base.js`.    
+`RichEditor` and `ReactCropper` will be packaged to `example.extend.js `.
+
+
+Function `Avatar`, `Button`, `RichEditor` and `ReactCropper` are added to global library `LIB`.
+And `LIB.Avatar`, `LIB.Button`, `LIB.RichEditor` and `LIB.ReactCropper` work well.
+
+
+## Options
+
+### mode
+
+Its value can be `add` or `replace` or ''. The default value is `add`.
+`add` means will add chunk file to entry.  
+`replace` means will remove original entry file, and add chunk file to entry.  
+'' means will not add chunk file to entry.  
+
+### min
+Its value must be boolean. The default is true.
+When it's true, it will add min to entry as above. Otherwise the `xx.min.js` will not add to entry.
+
+### chunkConfig
+Its value must be Array.
+You need to config it to split entry file into multiple files.
+
+
+## Example of usage on the Browser
+
+In the browser:
+
+```html
+<script src="https://cdn.xx.com/example.base.min.js"></script>
+<script src="https://cdn.xx.com/example.extend.min.js"></script>
+```
---
+++ index.js
@@ -0,0 +1,167 @@
+const path = require('path');
+const fs = require('fs');
+const PLUGIN_NAME = "EntryChunkWebpackPlugin";
+const warn = msg => console.warn(`[${PLUGIN_NAME}] ${msg}`);
+
+/**
+ * @class EntryChunkWebpackPlugin
+ */
+class EntryChunkWebpackPlugin {
+  /**
+   * @constructor
+   * @param {Object} [options]
+   * @param {String: 'add' | 'replace' | ''} [options.mode]
+   * @param {Boolean} [options.min]
+   * @param {Array({ name: string, modules: string[] })} [options.chunkConfig]
+   */
+  constructor(options) {
+    this._options = Object.assign({
+      mode: 'add', // 'add' || 'replace' || '',
+      min: true,
+      chunkConfig: [], // { name: string, modules: [] }
+    }, options);
+  }
+
+  /**
+   * @method apply
+   * @param {Compiler} compiler
+   */
+  apply(compiler) {
+    compiler.hooks.entryOption.tap(PLUGIN_NAME, (context, entry) => {
+      const { mode, chunkConfig } = this._options;
+      if (!chunkConfig || !chunkConfig.length) {
+        warn(`options.chunkConfig should not be empty.`)
+        return;
+      }
+
+      if (mode !== 'add' && mode !== 'replace' && mode !== '') {
+        warn(`options.mode should be 'add' or 'replace' or ''. Its default value is 'add'.`)
+        return;
+      }
+
+      let cloneEntry = Object.assign({}, entry);
+      if (mode === 'replace') {
+        for (let key in entry) {
+          delete entry[key];
+        }
+      }
+      let isResolvedMap = {};
+      for (let key in cloneEntry) {
+        const currentPath = cloneEntry[key];
+        let absolutePath = currentPath;
+        if (!path.isAbsolute(absolutePath)) {
+          absolutePath = path.resolve(context, absolutePath);
+        }
+
+        // If the path is resolved, then continue
+        if (isResolvedMap[absolutePath]) {
+          continue;
+        }
+        isResolvedMap[absolutePath] = true;
+
+        const source = fs.readFileSync(absolutePath).toString();
+
+        let noConfigModulesChunk = [];
+        let modulesInChunkMap = {};
+        chunkConfig.forEach((chunk) => {
+          const { name, modules } = chunk;
+          if (modules && modules.length > 0) {
+            // generate config modules file and add it to entry
+            splitEntryFileIntoChunkFile.bind(this)(
+              entry,
+              absolutePath,
+              name,
+              function (chunkEntryFilePath) {
+                outputFileByModules(source, chunkEntryFilePath, modules, modulesInChunkMap);
+              }
+            );
+          } else {
+            noConfigModulesChunk.push(chunk);
+          }
+        });
+
+        // generate no config modules file and add it to entry
+        noConfigModulesChunk.forEach((chunk) => {
+          splitEntryFileIntoChunkFile.bind(this)(
+            entry,
+            absolutePath,
+            chunk.name,
+            function (chunkEntryFilePath) {
+              outputNoConfigModulesFile(source, chunkEntryFilePath, modulesInChunkMap);
+            }
+          );
+        });
+      }
+    });
+  }
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function splitEntryFileIntoChunkFile(entry, entryFilePath, chunkName, outputFileCallback) {
+  const chunkEntryFilePath = generateChunkEntryFilePath(entryFilePath, chunkName);
+  outputFileCallback(chunkEntryFilePath);
+
+  const { mode } = this._options;
+  if (mode === 'add' || mode === 'replace') {
+    addEntry.bind(this)(entry, chunkName, chunkEntryFilePath);
+  }
+}
+
+function addEntry(entry, name, filePath) {
+  const { min } = this._options;
+
+  entry[name] = filePath;
+  if (min) {
+    entry[`${name}.min`] = filePath;
+  }
+}
+
+function generateMatchModuleRegExp(moduleName) {
+  return new RegExp(`export\\s+\\{\\s*default\\s+as\\s+${escapeRegExp(moduleName)}\\s*\\}\\s+from\\s+[']\\.?(.+)['];*`);
+}
+
+function outputFileByModules(source, filePath, modules, modulesInChunkMap) {
+  let modulesSource = [];
+  modules.forEach(function (moduleName) {
+    const matchModuleRegExp = generateMatchModuleRegExp(moduleName);
+    const matchModuleSource = source.match(matchModuleRegExp);
+    if (matchModuleSource) {
+      modulesSource.push(matchModuleSource[0]);
+      modulesInChunkMap[moduleName] = moduleName;
+    }
+  });
+
+  const entryChunkSource = modulesSource.join('\n') + '\n';
+  writeEntryChunkFileSync(filePath, entryChunkSource);
+}
+
+function outputNoConfigModulesFile(source, filePath, modulesInChunkMap) {
+  const modulesInChunk = Object.keys(modulesInChunkMap);
+  modulesInChunk.forEach(function (moduleName) {
+    const matchModuleRegExp = generateMatchModuleRegExp(moduleName);
+    source = source.replace(matchModuleRegExp, '');
+  });
+  source = source.replace(/\n{3,}/g, '\n\n');
+  writeEntryChunkFileSync(filePath, source);
+}
+
+function writeEntryChunkFileSync(filePath, source) {
+  // add ../ to source file path
+  source = source.replace(/[']((\.\/)|(\.\.\/))?(.+)(['];*)/g, '\'$1..\/$4$5');
+
+  const folderDir = filePath.match(/(.*\/)[^\/]+$/)[1];
+  if (!fs.existsSync(folderDir)) {
+    fs.mkdirSync(folderDir);
+  }
+  fs.writeFileSync(filePath, source);
+}
+
+function generateChunkEntryFilePath(filePath, name) {
+  return filePath.replace(/(.*\/)[^\/]+(\.[^\.^\/]+)$/, `$1webpack-entry-chunk\/${name}$2`);
+}
+
+// Exports
+module.exports = EntryChunkWebpackPlugin;
